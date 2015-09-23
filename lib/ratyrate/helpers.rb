4bd274b4d942564ddf01ac8c20474ba645d0fc1c
Yih Yang <yyngo788n88@hotmail.com>
2015-09-23 14:09:50 +0800

    Added an overall average method, pending optimizing

--- lib/ratyrate/helpers.rb
+++ lib/ratyrate/helpers.rb
@@ -71,6 +71,73 @@ module Helpers
     end
   end
 
+  # Overall average rating, read only
+  # TODO: Optimize codes, remove unused attributes
+  def overall_average_rating_for(rateable_obj, dimension=nil, options={})
+    avg = rateable_obj.get_overall_average
+    puts "average:" + avg.to_s
+
+    star         = options[:star]         || 5
+    enable_half  = options[:enable_half]  || false
+    half_show    = options[:half_show]    || true
+    star_path    = options[:star_path]    || ''
+    star_on      = options[:star_on]      || image_path('star-on.png')
+    star_off     = options[:star_off]     || image_path('star-off.png')
+    star_half    = options[:star_half]    || image_path('star-half.png')
+    cancel       = false
+    cancel_place = options[:cancel_place] || 'left'
+    cancel_hint  = options[:cancel_hint]  || 'Cancel current rating!'
+    cancel_on    = options[:cancel_on]    || image_path('cancel-on.png')
+    cancel_off   = options[:cancel_off]   || image_path('cancel-off.png')
+    noRatedMsg   = options[:noRatedMsg]   || 'I\'am readOnly and I haven\'t rated yet!'
+    # round        = options[:round]        || { down: .26, full: .6, up: .76 }
+    space        = options[:space]        || false
+    single       = options[:single]       || false
+    target       = options[:target]       || ''
+    targetText   = options[:targetText]   || ''
+    targetType   = options[:targetType]   || 'hint'
+    targetFormat = options[:targetFormat] || '{score}'
+    targetScore  = options[:targetScore]  || ''
+    readOnly     = true   # Set to read only
+    style        = options[:style]        || ''
+
+    disable_after_rate = options[:disable_after_rate] && true
+    disable_after_rate = true if disable_after_rate == nil
+
+    if disable_after_rate
+      readonly = !(current_user && rateable_obj.can_rate?(current_user, dimension))
+    else
+      readonly = !current_user || false
+    end
+
+    content_tag :div, '', "data-dimension" => dimension, :class => "star", "data-rating" => avg,
+                "data-id" => rateable_obj.id, "data-classname" => rateable_obj.class.name == rateable_obj.class.base_class.name ? rateable_obj.class.name : rateable_obj.class.base_class.name,
+                "data-disable-after-rate" => disable_after_rate,
+                "data-readonly" => readOnly,
+                "data-enable-half" => enable_half,
+                "data-half-show" => half_show,
+                "data-star-count" => star,
+                "data-star-path" => star_path,
+                "data-star-on" => star_on,
+                "data-star-off" => star_off,
+                "data-star-half" => star_half,
+                "data-cancel" => cancel,
+                "data-cancel-place" => cancel_place,
+                "data-cancel-hint"  => cancel_hint,
+                "data-cancel-on" => cancel_on,
+                "data-cancel-off" => cancel_off,
+                "data-no-rated-message" => noRatedMsg,
+                # "data-round" => round,
+                "data-space" => space,
+                "data-single" => single,
+                "data-target" => target,
+                "data-target-text" => targetText,
+                "data-target-type" => targetType,
+                "data-target-format" => targetFormat,
+                "data-target-score" => targetScore,
+                "style" => style
+
+  end
   def imdb_style_rating_for(rateable_obj, user, options = {})
     #TODO: add option to change the star icon
     overall_avg = rateable_obj.get_overall_average
@@ -79,6 +146,7 @@ module Helpers
         content_tag :p, overall_avg, :style => "position:relative;line-height:85px;text-align:center;"
     end
   end
+
 
   def rating_for_user(rateable_obj, rating_user, dimension = nil, options = {})
     @object = rateable_obj
